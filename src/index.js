f663d5aaafc9653ce432d8144f5c0bebea62600e
david muchiri <njugidav@gmail.com>
2018-12-18 02:09:48 +0300

    added webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.base.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			cache: true,
+			template: path.join(__dirname, 'src/index.html')
+		})
+	]
+};
